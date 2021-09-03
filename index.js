2e2909e039403ae0fdc2e9b796e78f4bf9cd216d
nyctonio <nyctonio.dev@gmail.com>
2021-09-03 23:35:20 +0530

    polygon created

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require('express');
 const bodyparser=require("body-parser");
 const port = process.env.PORT || 3000;
 const app = express();
+var cors = require('cors')
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(cors());
 app.set('view engine', 'ejs');
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(bodyparser.json());
